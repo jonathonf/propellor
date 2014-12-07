b67bd46361d2a4afdadce546955f83d314a7739b
Jonathon Fernyhough <jonathon@awyr.co.uk>
2014-12-07 16:06:54 +0000

    propellor spin

--- config-simple.hs
+++ config-simple.hs
@@ -7,9 +7,9 @@ import Propellor.Property.Scheduled
 import qualified Propellor.Property.File as File
 import qualified Propellor.Property.Apt as Apt
 import qualified Propellor.Property.Network as Network
---import qualified Propellor.Property.Ssh as Ssh
+import qualified Propellor.Property.Ssh as Ssh
 import qualified Propellor.Property.Cron as Cron
---import qualified Propellor.Property.Sudo as Sudo
+import qualified Propellor.Property.Sudo as Sudo
 import qualified Propellor.Property.User as User
 --import qualified Propellor.Property.Hostname as Hostname
 --import qualified Propellor.Property.Reboot as Reboot
